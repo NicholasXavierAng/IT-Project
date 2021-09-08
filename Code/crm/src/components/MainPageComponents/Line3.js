4ae4b9edac630095e4097b7c3f67e980b87934de
quanganht <79823839+quanganht@users.noreply.github.com>
2021-09-08 23:25:21 +1000

    Contain "Create Button" and labels
    Labels: "name", "progress", "priority"

---
+++ Code/crm/src/components/MainPageComponents/Line3.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Addcontact = () => {
+    return (
+        <div className ='line3'>
+            <div>
+                <button className="burgundyButton" style = {{paddingLeft: '50px', color: 'white'}}>CREATE CONTACT</button>
+            </div>
+            <div className="titles">
+                <p className="p">Name</p> 
+                <p className="p">Status</p>  
+                <p className="p">Progress</p>
+                <svg width="21" height="14" viewBox="0 0 21 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 14H7V11.6667H0V14ZM0 0V2.33333H21V0H0ZM0 8.16667H14V5.83333H0V8.16667Z" fill="black"/>
+                </svg>
+            </div>
+    
+        </div>
+    )
+}
+
+export default Addcontact
+/* Vector */
+
