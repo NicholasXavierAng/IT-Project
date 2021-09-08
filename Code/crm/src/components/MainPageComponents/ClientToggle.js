19bad07cf42db927bb40ed9b059bcefea18ca930
quanganht <79823839+quanganht@users.noreply.github.com>
2021-09-08 23:22:57 +1000

    JS file for the client buttons

---
+++ Code/crm/src/components/MainPageComponents/ClientToggle.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+
+
+const ClientToggle = () => {
+    return (
+        <div className = "lowerpart">
+            <div className = "sidebar"/>
+            <div className = "clients" >
+                <button className = "client" onClick={() => {console.log("To client's profile")}}>
+                    <p className = "name">John Doe</p>
+                    <p className = "status">new</p>
+                    <p className = "progress">high</p>
+                </button>
+                <svg width="1104" height="4" viewBox="0 0 1104 4" fill="none" xmlns="http://www.w3.org/2000/svg" >
+                <path d="M0 2L1104 2.35158" stroke="#C4C4C4" stroke-opacity="0.5" stroke-width="2.08177"/>
+                </svg>
+                <button className = "client">
+                    <p className = "name">John Doe</p>
+                    <p className = "status">new</p>
+                    <p className = "progress">high</p>
+                </button>
+                <svg width="1104" height="4" viewBox="0 0 1104 4" fill="none" xmlns="http://www.w3.org/2000/svg" >
+                <path d="M0 2L1104 2.35158" stroke="#C4C4C4" stroke-opacity="0.5" stroke-width="2.08177"/>
+                </svg>
+                <button className = "client">
+                    <p className = "name">John Doe</p>
+                    <p className = "status">new</p>
+                    <p className = "progress">high</p>
+                </button>
+                <svg width="1104" height="4" viewBox="0 0 1104 4" fill="none" xmlns="http://www.w3.org/2000/svg" >
+                <path d="M0 2L1104 2.35158" stroke="#C4C4C4" stroke-opacity="0.5" stroke-width="2.08177"/>
+                </svg>
+                <button className = "client">
+                    <p className = "name">John Doe</p>
+                    <p className = "status">new</p>
+                    <p className = "progress">high</p>
+                </button>
+                <svg width="1104" height="4" viewBox="0 0 1104 4" fill="none" xmlns="http://www.w3.org/2000/svg" >
+                <path d="M0 2L1104 2.35158" stroke="#C4C4C4" stroke-opacity="0.5" stroke-width="2.08177"/>
+                </svg>
+                <button className = "client">
+                    <p className = "name">John Doe</p>
+                    <p className = "status">new</p>
+                    <p className = "progress">high</p>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+export default ClientToggle
