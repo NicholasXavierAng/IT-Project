6e354178c2d2d705189792863ead2e8ebd6048ae
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2021-09-13 00:21:31 +0500

    Fixed the profile route

--- Code/crm/src/components/App/App.js
+++ Code/crm/src/components/App/App.js
@@ -30,7 +30,7 @@ function App() {
                         )}>
                         </Route>
                         {/* User Home Page Route  */}
-                        <Route path="/user">
+                        <Route path="/user" exact>
                             <UserHome/>
                         </Route>
                         {/* User Profile Route*/}
